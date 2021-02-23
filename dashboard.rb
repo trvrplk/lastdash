76b9eeed6c73a3308de094a5ba3357c2242f04f7
kewlpinguino <keymint1498@gmail.com>
2021-02-23 13:52:40 -0500

    Added avatar for 'you' on the 'all dashboard'

--- dashboard.rb
+++ dashboard.rb
@@ -25,6 +25,11 @@ def get_friends(user)
   $api_key}&format=xml").read)
 end
 
+def get_info(user)
+  Nori.new.parse(URI.open("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=#{user}&api_key=#{
+  $api_key}&format=xml").read)
+end
+
 get '/you/:user' do
   tracks = get_recently_played("#{params[:user]}", 10)["lfm"]["recenttracks"]["track"]
   haml :you_dashboard,  :layout => :default_layout, :locals => {:tracks => tracks, :user => "#{params[:user]}"} 
@@ -45,5 +50,6 @@ get '/all/:user' do
   end
   
   friends = get_friends("#{params[:user]}")["lfm"]["friends"]["user"]
-  haml :all_dashboard, :layout => :default_layout, :locals => {:your_track => @your_track, :friends => friends, :last_five => @last_five}
+  your_avatar = get_info("#{params[:user]}")["lfm"]["user"]["image"][1]
+  haml :all_dashboard, :layout => :default_layout, :locals => {:your_track => @your_track, :friends => friends, :last_five => @last_five, :your_avatar => your_avatar}
 end
